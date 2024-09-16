bba93d5321e0cc739bf8b74562bf93be065c1db6
aggrigor <agvangrigoryan2003@gmail.com>
2024-09-16 23:27:28 +0400

    ex00 done

---
+++ CPP_03/ex00/ClapTrap.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "ClapTrap.hpp"
+
+// Default Constructor
+ClapTrap::ClapTrap() : name("<anonymous>"), hit_points(10), energy_points(10), attack_damage(0) {
+	std::cout << "Default Constructor called" << std::endl;
+}
+
+// Paramether Constructor
+ClapTrap::ClapTrap(const std::string& new_name) : name(new_name), hit_points(10), energy_points(10), attack_damage(0) {
+	std::cout << "Parameter Constructor called" << std::endl;
+};
+
+// Copy constructor
+ClapTrap::ClapTrap(const ClapTrap& other) {
+	if (this == &other) return;
+
+	name = other.name;
+	hit_points = other.hit_points;
+	energy_points = other.energy_points;
+	attack_damage = other.attack_damage;
+
+	std::cout << "Copy Constructor called" << std::endl;
+}
+
+// Copy assigment operator
+ClapTrap& ClapTrap::operator=(const ClapTrap& other) {
+	if (this == &other) return (*this);
+
+	name = other.name;
+	hit_points = other.hit_points;
+	energy_points = other.energy_points;
+	attack_damage = other.attack_damage;
+
+	std::cout << "Copy assigment operator called" << std::endl;
+	return (*this);
+}
+
+// Destructor
+ClapTrap::~ClapTrap() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+// Member functions
+void ClapTrap::attack(const std::string& target) {
+	if (energy_points <= 0) {
+		std::cout << "Not enough energy points to attack" << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << name << " attacks " << target << ", causing " << attack_damage << " points of damage!" << std::endl;
+	energy_points--;
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+	hit_points -= amount;
+	std::cout << name << " took " << amount << " damage" << std::endl;
+	if (hit_points <= 0) {
+		hit_points = 0;
+		std::cout << name << " die" << std::endl;;
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+	if (energy_points <= 0) {
+		std::cout << "Not enough energy points to repair" << std::endl;
+		return ;
+	}
+	hit_points += amount;
+	energy_points--;
+	std::cout << name << " repaired " << amount << " hit points" << std::endl;
+}
---
+++ CPP_03/ex00/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap ct("PEDRO");
+	ct.attack("HOPAR");
+	ct.attack("HOPAR");
+	ct.attack("HOPAR");
+	ct.attack("HOPAR");
+	ct.attack("HOPAR");
+	ct.attack("HOPAR");
+	ct.attack("HOPAR");
+	ct.attack("HOPAR");
+	ct.attack("HOPAR");
+	// ct.attack("HOPAR");
+	ct.beRepaired(10);
+}
