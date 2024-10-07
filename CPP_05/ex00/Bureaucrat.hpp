14ea2ee5eb8ec7886a2c3ab75dcf137c58dd0af6
aggrigor <agvangrigoryan2003@gmail.com>
2024-10-07 13:03:31 +0400

    ex00 of cpp04 done

---
+++ CPP_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,37 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+#define LOWEST_GRADE 150
+#define HIGHEST_GRADE 1
+
+class Bureaucrat {
+private:
+	const std::string	_name;
+	short				_grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string& name, short grade);
+	Bureaucrat(const Bureaucrat& other);
+	~Bureaucrat();
+
+	class GradeTooHighException : std::exception {};
+	class GradeTooLowException : std::exception {};
+
+// member functions
+	Bureaucrat& operator=(const Bureaucrat& other);
+	void	downGrade();
+	void	raiseGrade();
+// getters
+	std::string	getName();
+	short		getGrade();
+
+// setters
+	void		setGrade(const short grade);
+};
+
+std::ostream& operator<<(std::ostream& outsteram, Bureaucrat& obj);
+
+#endif
