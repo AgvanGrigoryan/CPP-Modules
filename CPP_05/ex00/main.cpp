84bb36dcb2b31fec6f4d9fe0eb45c7dc55601b47
aggrigor <agvangrigoryan2003@gmail.com>
2024-10-08 17:28:37 +0400

    update exercise tests

--- CPP_05/ex00/main.cpp
+++ CPP_05/ex00/main.cpp
@@ -1,14 +1,12 @@
 #include "Bureaucrat.hpp"
 #include <iostream>
 
-
 int	main(void) {
-	Bureaucrat* ponch = NULL;
-
+//	Testing bureaucrat set negative grade
+{
+	std::cout << "\033[0;32m" << "Testing bureaucrat set negative grade" << "\033[0m" << std::endl;
 	try {
-		ponch = new Bureaucrat("Armenchik", 2);
-		ponch->raiseGrade();
-		ponch->raiseGrade();
+		Bureaucrat ponch("Armenchik", -89);
 	}
 	catch (Bureaucrat::GradeTooHighException& e) {
 		std::cout << "\033[0;031m" << "New Grade is too high!" << "\033[0m" << std::endl;
@@ -16,8 +14,80 @@ int	main(void) {
 	catch (Bureaucrat::GradeTooLowException& e) {
 		std::cout << "\033[0;031m" << "New Grade is too low!" << "\033[0m" << std::endl;
 	}
+}
+std::cout << '\n';
 
-	if (ponch != NULL)
-		std::cout << *ponch << std::endl;
-	delete ponch;
+//	Testing bureaucrat set too high grade
+{
+	std::cout << "\033[0;32m" << "Testing bureaucrat set too high grade" << "\033[0m" << std::endl;
+	try {
+		Bureaucrat ponch("Armenchik", 777);
+	}
+	catch (Bureaucrat::GradeTooHighException& e) {
+		std::cout << "\033[0;031m" << "New Grade is too high!" << "\033[0m" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException& e) {
+		std::cout << "\033[0;031m" << "New Grade is too low!" << "\033[0m" << std::endl;
+	}
+}
+std::cout << '\n';
+
+//	Testing bureaucrat grade incrementing
+{
+	std::cout << "\033[0;32m" << "Testing bureaucrat grade incrementing" << "\033[0m" << std::endl;
+	Bureaucrat ponch("Armenchik", 2);
+	try {
+		std::cout << ponch << std::endl;
+		ponch.raiseGrade();
+		std::cout << ponch << std::endl;
+		ponch.raiseGrade();
+		std::cout << ponch << std::endl;
+	}
+	catch (Bureaucrat::GradeTooHighException& e) {
+		std::cout << "\033[0;031m" << "New Grade is too high!" << "\033[0m" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException& e) {
+		std::cout << "\033[0;031m" << "New Grade is too low!" << "\033[0m" << std::endl;
+	}
+}
+std::cout << '\n';
+
+//	Testing bureaucrat grade DEcrementing
+{
+	std::cout << "\033[0;32m" << "Testing bureaucrat grade DEcrementing" << "\033[0m" << std::endl;
+	Bureaucrat ponch("Armenchik", 149);
+	try {
+		std::cout << ponch << std::endl;
+		ponch.downGrade();
+		std::cout << ponch << std::endl;
+		ponch.downGrade();
+		std::cout << ponch << std::endl;
+	}
+	catch (Bureaucrat::GradeTooHighException& e) {
+		std::cout << "\033[0;031m" << "New Grade is too high!" << "\033[0m" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException& e) {
+		std::cout << "\033[0;031m" << "New Grade is too low!" << "\033[0m" << std::endl;
+	}
+}
+std::cout << '\n';
+
+//	Testing bureaucrat normal grade
+{
+	std::cout << "\033[0;32m" << "Testing bureaucrat normal grade" << "\033[0m" << std::endl;
+	try {
+		Bureaucrat ponch("Armenchik", 75);
+		std::cout << ponch << std::endl;
+		ponch.downGrade();
+		std::cout << ponch << std::endl;
+		ponch.raiseGrade();
+		std::cout << ponch << std::endl;
+	}
+	catch (Bureaucrat::GradeTooHighException& e) {
+		std::cout << "\033[0;031m" << "New Grade is too high!" << "\033[0m" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException& e) {
+		std::cout << "\033[0;031m" << "New Grade is too low!" << "\033[0m" << std::endl;
+	}
+}
 }
