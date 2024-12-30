51bd0ad495204db3e6c0678107bdd37b8989c739
Agvan <101641443+AgvanGrigoryan@users.noreply.github.com>
2024-12-30 13:55:47 +0400

    cpp_07 done

---
+++ CPP_07/whatever.hpp
@@ -0,0 +1,26 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T> void swap(T &left, T &right) {
+	T tmp;
+
+	tmp = left;
+	left = right;
+	right = tmp;
+}
+
+template <typename T> T min(T &left, T &right) {
+	if (left < right)
+		return (left);
+	else
+		return (right);
+}
+
+template <typename T> T max(T &left, T &right) {
+	if (left > right)
+		return (left);
+	else
+		return (right);
+}
+
+#endif
