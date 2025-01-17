f9151ed06397a549bce98f8e62a6b3dbbcf33dbc
aggrigor <agvangrigoryan2003@gmail.com>
2025-01-17 13:15:24 +0400

    done

--- CPP_07/ex00/whatever.hpp
+++ CPP_07/ex00/whatever.hpp
@@ -1,22 +1,23 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-template <typename T> void swap(T &left, T &right) {
-	T tmp;
-
-	tmp = left;
+template <typename T>
+void swap(T &left, T &right) {
+	T tmp = left;
 	left = right;
 	right = tmp;
 }
 
-template <typename T> T min(T &left, T &right) {
+template <typename T>
+T& min(T &left, T &right) {
 	if (left < right)
 		return (left);
 	else
 		return (right);
 }
 
-template <typename T> T max(T &left, T &right) {
+template <typename T>
+T& max(T &left, T &right) {
 	if (left > right)
 		return (left);
 	else
