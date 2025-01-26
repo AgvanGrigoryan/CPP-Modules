efb98dad38f08848f0a1e66035d968a2c7029eb1
Agvan <101641443+AgvanGrigoryan@users.noreply.github.com>
2025-01-26 17:44:48 +0400

    changed throwing error message

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -7,7 +7,7 @@ template <typename T>
 typename  T::const_iterator	easyfind(const T &container_of_ints, int integer) {
 	typename T::const_iterator it = std::find(container_of_ints.begin(), container_of_ints.end(), integer);
 	if (it == container_of_ints.end())
-		throw std::exception();
+		throw std::runtime_error("Not found!");
 	return (it);
 }
 
@@ -15,7 +15,7 @@ template <typename T>
 typename  T::iterator	easyfind(T &container_of_ints, int integer) {
 	typename T::iterator it = std::find(container_of_ints.begin(), container_of_ints.end(), integer);
 	if (it == container_of_ints.end())
-		throw std::exception();
+		throw std::runtime_error("Not found!");
 	return (it);
 }
 
